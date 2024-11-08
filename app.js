cc86ec746d56ffeb1a7cbe9272a9ed28bd2ca340
PruebaDeArchivos <archivos15246@gmail.com>
2024-11-07 21:37:32 -0500

    Clase 2

---
+++ app.js
@@ -0,0 +1,13 @@
+var nombre = prompt("Cual es tu nombre?")
+var nameDom = document.getElementById("nameDom")
+nameDom.textContent = "Hola " +  nombre
+//console.log(nombre)
+
+var Saaa
+
+var Saaa
+var Saaa
+var Saaa
+var Saaa
+
+
